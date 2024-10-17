fbea1d7f7351c321ef43cd28ccd12053693fc00c
2tank <adrianfeve7@gmail.com>
2024-10-17 10:54:40 +0200

    React Router DOM

--- Awita/src/App.jsx
+++ Awita/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
+<<<<<<< Updated upstream
 
   return (
     <h1 className='text-center'>
       AWITA u.u
     </h1>
   )
+=======
+ 
+  return(
+
+    <Router>
+      <Routes>
+        <Route path="/" element></Route>
+      </Routes>
+    </Router>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
